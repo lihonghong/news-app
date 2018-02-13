57bffb156e2a63a614b2dbefcb149c6f69288fc0
lihonghong <lihonghong@xiaomi.com>
2018-02-13 10:27:47 +0800

    fix mysql

--- news-app-web/app.js
+++ news-app-web/app.js
@@ -26,3 +26,5 @@ app.use('/', routes);
 http.createServer(app).listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
 });
+
+module.exports = app;
